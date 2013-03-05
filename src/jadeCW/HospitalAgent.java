ec9c7ccb6509b4395a2b776d92c32f0fd1091579
Alessandro Galli <ag6609@doc.ic.ac.uk>
2013-03-05 16:23:40 +0000

    get this jamal

--- src/jadeCW/HospitalAgent.java
+++ src/jadeCW/HospitalAgent.java
@@ -15,25 +15,29 @@ public class HospitalAgent extends Agent {
 
 		System.out.println("Created hospital agent");
 		
-		int totalAppointments;
+		int totalAppointments = 0;
 		
 	  	Object[] args = getArguments();
 	  	if (args != null && args.length > 0) {
-	  		totalAppointments = (Integer) args[0];
+	  		totalAppointments = Integer.parseInt((String) args[0]);
 	  	}
 	  	
+        String serviceName = "appointment-allocator";
+        String serviceType = "allocate-appointments";
+	  	
 	  	// Register the service
-	  	System.out.println("Agent "+getLocalName()+" registering service of type \"allocate-appointments\"");
+	  	System.out.println("Agent "+getLocalName()+" registering service \"" +serviceName
+	  			+"\" of type \"allocate-appointments\"");
 	  	try {
 	  		DFAgentDescription dfd = new DFAgentDescription();
 	  		dfd.setName(getAID());
 	  		ServiceDescription sd = new ServiceDescription();
-	  		//sd.setName(serviceName);
-	  		sd.setType("allocate-appointments");
+	  		sd.setName(serviceName);
+	  		sd.setType(serviceType);
 	  		// Agents that want to use this service need to "know" the weather-forecast-ontology
 	  		//sd.addOntologies("weather-forecast-ontology");
 	  		// Agents that want to use this service need to "speak" the FIPA-SL language
-	  		//sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
+	  		sd.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
 	  		//sd.addProperties(new Property("country", "Italy"));
 	  		dfd.addServices(sd);
 	  		
